c2a3c97b2249fbe1a392588ed791307272341574
Vladimir Steshin <warheim24@gmail.com>
2021-06-11 12:19:11 +0300

    Lecture grades added

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ class Student:
         self.grades = {}
         self.grade_stud_average = []
 
+    def lecturer_grade(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return ('Ошибка')
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -49,7 +59,12 @@ strict_reviewer = Reviewer("Robert", "Bolson")
 strict_reviewer.courses_attached += ['Python']
 strict_reviewer.courses_attached += ['Введение в программирование']
 
+best_student.lecturer_grade(some_lecturer, 'Python', 10)
+best_student.lecturer_grade(some_lecturer, 'Python', 8)
+best_student.lecturer_grade(some_lecturer, 'Python', 9)
+
 strict_reviewer.rate_hw(best_student, 'Python', 10)
 strict_reviewer.rate_hw(best_student, 'Python', 7)
 strict_reviewer.rate_hw(best_student, 'Python', 9)
 
+print(some_lecturer.grades)
