66f96d5534d4fc7f149e700a4b91a4b558402ce0
Vladimir Steshin <warheim24@gmail.com>
2021-06-11 13:57:46 +0300

    Trying to fix Grade = 0

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ class Student:
             return 'Ошибка'
 
     def student_grade_sum(self, student):
-        grade_sum = 0
-        grade_len = 0
         if isinstance(student, Student):
             for number in self.grades:
                 grade_sum = sum(self.grades[number])
-            for count in self.grades.values():
-                grade_len = len(count)
-            self.grade_stud_average = round(grade_sum / grade_len, 1)
-            return
+                for count in self.grades.values():
+                    grade_len = len(count)
+                    if grade_sum != 0 and grade_len != 0:
+                        self.grade_stud_average = round(grade_sum / grade_len, 1)
+                    else:
+                        return 'Ошибка'
 
     def __str__(self):
         super().__str__()
@@ -62,15 +62,15 @@ class Lecturer(Mentor):
         self.grade_lect_average = []
 
     def lector_grade_sum(self, lecturer):
-        grade_sum = 0
-        grade_len = 0
         if isinstance(lecturer, Lecturer):
             for number in self.grades:
                 grade_sum = sum(self.grades[number])
-            for count in self.grades.values():
-                grade_len = len(count)
-            self.grade_lect_average = round(grade_sum / grade_len, 1)
-            return
+                for count in self.grades.values():
+                    grade_len = len(count)
+                    if grade_sum != 0 and grade_len != 0:
+                        self.grade_lect_average = round(grade_sum / grade_len, 1)
+                    else:
+                        return 'Ошибка'
 
     def __str__(self):
         res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредний балл: {self.grade_lect_average}'
@@ -91,8 +91,15 @@ best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Git']
 best_student.finished_courses += ['Введение в программирование']
 
+worst_student = Student('Max', 'Payne', 'male')
+worst_student.courses_in_progress += ['Python']
+worst_student.finished_courses += ['Введение в программирование']
+
 some_lecturer = Lecturer("Some", "Buddy")
 some_lecturer.courses_attached += ['Python']
+
+another_lecturer = Lecturer('Forrest', 'Gump')
+another_lecturer.courses_attached += ['Введение в программирование']
 
 strict_reviewer = Reviewer("Robert", "Bolson")
 strict_reviewer.courses_attached += ['Python']
@@ -102,13 +109,38 @@ best_student.lecturer_grade(some_lecturer, 'Python', 10)
 best_student.lecturer_grade(some_lecturer, 'Python', 8)
 best_student.lecturer_grade(some_lecturer, 'Python', 9)
 
+best_student.lecturer_grade(another_lecturer, 'Введение в программирование', 7)
+best_student.lecturer_grade(another_lecturer, 'Введение в программирование', 8)
+best_student.lecturer_grade(another_lecturer, 'Введение в программирование', 6)
+
+worst_student.lecturer_grade(some_lecturer, 'Python', 6)
+worst_student.lecturer_grade(some_lecturer, 'Python', 5)
+worst_student.lecturer_grade(some_lecturer, 'Python', 5)
+
+worst_student.lecturer_grade(another_lecturer, 'Введение в программирование', 6)
+worst_student.lecturer_grade(another_lecturer, 'Введение в программирование', 4)
+worst_student.lecturer_grade(another_lecturer, 'Введение в программирование', 7)
+
 strict_reviewer.rate_hw(best_student, 'Python', 10)
 strict_reviewer.rate_hw(best_student, 'Python', 7)
 strict_reviewer.rate_hw(best_student, 'Python', 9)
 
+strict_reviewer.rate_hw(worst_student, 'Введение в программирование', 3)
+strict_reviewer.rate_hw(worst_student, 'Введение в программирование', 4)
+strict_reviewer.rate_hw(worst_student, 'Введение в программирование', 5)
+
 some_lecturer.lector_grade_sum(some_lecturer)
 best_student.student_grade_sum(best_student)
+another_lecturer.lector_grade_sum(another_lecturer)
+worst_student.student_grade_sum(worst_student)
 
 print(some_lecturer)
-print('')
+print()
+print(another_lecturer)
+print()
 print(best_student)
+print()
+print(worst_student)
+
+# print(worst_student.grades)
+# print(another_lecturer.grades)
